2c82a1b757ef916ba77792cd7879d56cf660a642
Mircea Dan <mircea-dan.gheorghe@keysight.com>
2023-04-27 14:07:42 -0700

    update

--- generated/test_mirror_session.py
+++ generated/test_mirror_session.py
@@ -4,7 +4,7 @@ import pytest
 
 
 class TestSaiMirrorSession:
-    # object with parent SAI_OBJECT_TYPE_PORT, SAI_OBJECT_TYPE_LAG, SAI_OBJECT_TYPE_SYSTEM_PORT SAI_OBJECT_TYPE_PORT, SAI_OBJECT_TYPE_LAG, SAI_OBJECT_TYPE_SYSTEM_PORT
+    # object with parent SAI_OBJECT_TYPE_PORT SAI_OBJECT_TYPE_LAG SAI_OBJECT_TYPE_SYSTEM_PORT SAI_OBJECT_TYPE_PORT SAI_OBJECT_TYPE_LAG SAI_OBJECT_TYPE_SYSTEM_PORT
 
     @pytest.mark.dependency(scope='session')
     def test_mirror_session_create(self, npu):
